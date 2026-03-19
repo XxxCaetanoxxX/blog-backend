4442c0df2d944451176d8485e12e6bafd4acffb3
caetano.cesar <caetano.cesar@defensoria.mg.def.br>
2026-03-19 09:26:06 -0300

    criando modulos

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
+import { UserModule } from './user/user.module';
+import { PostModule } from './post/post.module';
 
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, UserModule, PostModule],
   providers: [AppService],
   controllers: [AppController],
 })
