cf9c83b5649c17ba76bee3850176b6c09cd69434
Caetano César <caetanocesar35@gmail.com>
2026-03-27 14:56:55 -0300

    endpoints post

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req, ParseUUIDPipe } from '@nestjs/common';
 import { PostService } from './post.service';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import type { AuthenticatedRequest } from 'src/auth/types/authenticated-request';
+import { PostResponseDto } from './dto/post-response.dto';
 
 @Controller('post')
 export class PostController {
@@ -16,6 +17,36 @@ export class PostController {
     @Body() createPostDto: CreatePostDto
   ) {
     return await this.postService.crate(createPostDto, req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Patch('me/:id')
+  async update(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Req() req: AuthenticatedRequest,
+    @Body() updatePostDto: UpdatePostDto
+  ) {
+    const post = await this.postService.update({ id }, updatePostDto, req.user);
+    return new PostResponseDto(post);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('/me/:id')
+  async findOneOwned(
+    @Req() req: AuthenticatedRequest,
+    @Param('id', ParseUUIDPipe) id: string
+  ) {
+    const post = await this.postService.findOneOwnedOrFail({ id }, req.user);
+    return new PostResponseDto(post);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('/me')
+  async findAllOwned(
+    @Req() req: AuthenticatedRequest,
+  ) {
+    const posts = await this.postService.findAllOwned(req.user);
+    return posts.map(post => new PostResponseDto(post));
   }
 
 }
--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable, Logger } from '@nestjs/common';
+import { BadRequestException, Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
 import { Repository } from 'typeorm';
@@ -16,6 +16,66 @@ export class PostService {
         @InjectRepository(Post)
         private readonly postRepository: Repository<Post>
     ) { }
+
+
+    async findOneOrFail(postData: Partial<Post>) {
+        const post = await this.findOne(postData);
+
+        if (!post) {
+            throw new NotFoundException('Post nao encontrado');
+        }
+
+        return post;
+    }
+
+    async findOne(postData: Partial<Post>) {
+        const post = await this.postRepository.findOne({
+            where: postData,
+            relations: ['author']
+        });
+
+        return post;
+    }
+
+    async findOneOwned(postData: Partial<Post>, author: User) {
+        const post = await this.postRepository.findOne({
+            where: {
+                ...postData,
+                author: {
+                    id: author.id
+                }
+            },
+            relations: ['author']
+        });
+
+        return post;
+    }
+
+    async findOneOwnedOrFail(postData: Partial<Post>, author: User) {
+        const post = await this.findOneOwned(postData, author);
+
+        if (!post) {
+            throw new NotFoundException('Post nao encontrado');
+        }
+
+        return post;
+    }
+
+    async findAllOwned(author: User) {
+        const posts = await this.postRepository.find({
+            where:{
+                author:{
+                    id: author.id
+                }
+            },
+            order:{
+                createdAt: 'DESC'
+            },
+            relations: ['author']
+        });
+
+        return posts;
+    }
 
     async crate(createPostDto: CreatePostDto, author: User) {
         const post = this.postRepository.create({
@@ -38,5 +98,21 @@ export class PostService {
         });
 
         return new PostResponseDto(created);
+    }
+
+    async update(postData: Partial<Post>, dto: UpdatePostDto, author: User) {
+        if(Object.keys(dto).length === 0) {
+            throw new BadRequestException('Nenhum dado para atualizar');
+        }
+
+        const post = await this.findOneOwnedOrFail(postData, author);
+
+        post.title = dto.title ?? post.title;
+        post.content = dto.content ?? post.content;
+        post.excerpt = dto.excerpt ?? post.excerpt;
+        post.coverImageUrl = dto.coverImageUrl ?? post.coverImageUrl;
+        post.published = dto.published ?? post.published;
+
+        return this.postRepository.save(post);
     }
 }
